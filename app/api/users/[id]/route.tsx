f6b7d69d99eba55916d67f6f5e7c7e14a91a0919
sulemanAli-Dev <suleman.ali@eurustechnologies.com>
2024-03-15 11:25:51 +0500

    user CRUD operation is completed

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,22 +14,35 @@ export async function GET(
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await req.json();
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+  const updated_user = await prisma.user.update({
+    where: { id: parseInt(params.id) },
+    data: { email: body.email, name: body.name },
+  });
+  return NextResponse.json(updated_user, { status: 200 });
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params: { id } }: { params: { id: number } }
+  { params: { id } }: { params: { id: string } }
 ) {
   const body = await req.json();
-  if (id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!user)
     return NextResponse.json({ error: "User not exists" }, { status: 400 });
-  return NextResponse.json({}, { status: 200 });
+  const deletedUser = await prisma.user.delete({
+    where: { id: parseInt(id) },
+  });
+  return NextResponse.json(deletedUser, { status: 200 });
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -11,5 +11,13 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  const new_user = await prisma.user.create({
+    data: { name: body.name, email: body.email },
+  });
+  return NextResponse.json(new_user, { status: 200 });
 }
